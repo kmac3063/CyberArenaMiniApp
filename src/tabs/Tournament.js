0e41eabc0fc692a7cae84a68b3a028bdfc93e730
kmac3063 <lancovlancov@mail.ru>
2020-12-03 12:36:03 +1000

    test

--- src/tabs/Tournament.js
+++ src/tabs/Tournament.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Group} from "@vkontakte/vkui";
 
 const Tournament = () => {
-    return <Group>Tournament 1</Group>
+    return <Group>Tournament 2</Group>
 }
 
 export default Tournament
